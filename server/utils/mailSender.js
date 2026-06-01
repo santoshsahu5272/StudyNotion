b996422368f3a7a67afa2102ce29a572b1735768
Santosh Sahu <sahu75761@gmail.com>
2026-06-01 08:55:42 +0530

    fix: remove spaces from MAIL_PASS in .env

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -8,11 +8,12 @@ const mailSender = async (email, title, body) => {
     
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: false,
     })
 
     let info = await transporter.sendMail({
