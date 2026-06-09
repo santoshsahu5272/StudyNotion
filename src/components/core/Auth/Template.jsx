30fd22ff071b5632438ecc57f4ce3b95346363db
Santosh Sahu <sahu75761@gmail.com>
2026-06-09 16:51:30 +0530

    fix: login page image frame alignment

--- src/components/core/Auth/Template.jsx
+++ src/components/core/Auth/Template.jsx
@@ -32,6 +32,7 @@ function Template({ title, description1, description2, image, formType }) {
               width={558}
               height={504}
               loading="lazy"
+              className="absolute inset-0"
             />
             <img
               src={image}
@@ -39,7 +40,7 @@ function Template({ title, description1, description2, image, formType }) {
               width={558}
               height={504}
               loading="lazy"
-              className="absolute -top-4 right-4 z-10"
+              className="relative z-10"
             />
           </div>
         </div>
